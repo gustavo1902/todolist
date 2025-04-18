0e11c01d5e7e0bda43ad562d1d45cec37a9f94f1
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-18 20:10:26 -0300

    feat: id jakarta

--- src/main/java/com/gustavo/todolist/user/UserModel.java
+++ src/main/java/com/gustavo/todolist/user/UserModel.java
@@ -1,11 +1,17 @@
 package com.gustavo.todolist.user;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    private UUID id;
 
     private String username;
     private String name;

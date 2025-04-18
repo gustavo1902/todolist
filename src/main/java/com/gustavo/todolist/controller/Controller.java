10744f43261bb04553142a89f23eb877dfe719ea
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-18 18:00:24 -0300

    feat: mensagem

--- src/main/java/com/gustavo/todolist/controller/Controller.java
+++ src/main/java/com/gustavo/todolist/controller/Controller.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    
+    public String mensagem() {
+        return "mensagem";
+    }
     
 }

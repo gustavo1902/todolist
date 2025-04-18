529f5f4c9c6b16d0457626897a4720747f275c34
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-18 18:15:17 -0300

    feat: add notation RestController and RequestMapping

--- src/main/java/com/gustavo/todolist/user/UserController.java
+++ src/main/java/com/gustavo/todolist/user/UserController.java
@@ -1,5 +1,10 @@
 package com.gustavo.todolist.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
     
 }

a41aadd303503eec2cc8d879f42230a93fd0a1d6
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-18 20:09:19 -0300

    feat: entity

--- src/main/java/com/gustavo/todolist/user/UserModel.java
+++ src/main/java/com/gustavo/todolist/user/UserModel.java
@@ -1,8 +1,10 @@
 package com.gustavo.todolist.user;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
 
     private String username;

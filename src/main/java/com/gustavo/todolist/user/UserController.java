49480f462a158843f42fc87a565f25d674bfeefd
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-18 22:31:34 -0300

    feat: UserCreated

--- src/main/java/com/gustavo/todolist/user/UserController.java
+++ src/main/java/com/gustavo/todolist/user/UserController.java
@@ -1,17 +1,21 @@
 package com.gustavo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
-        
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);   
+        return userCreated;     
     }
     
 }

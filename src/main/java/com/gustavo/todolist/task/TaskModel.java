2aba49de4964fa51f45e5c0078b0ec342b9a678c
gustavo1902 <gustavomorais1009@gmail.com>
2025-04-20 23:45:09 -0300

    fix: tabela

--- src/main/java/com/gustavo/todolist/task/TaskModel.java
+++ src/main/java/com/gustavo/todolist/task/TaskModel.java
@@ -1,28 +1,27 @@
 package com.gustavo.todolist.task;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name = "tb_tasks")
+@Entity(name="tb_tasks")
 public class TaskModel {
-
-    @Id
+    
+    @Id 
     @GeneratedValue(generator = "UUID")
     private UUID id;
-
-    @Column(length = 50)
-    private String title;
     private String description;
+    
+    @Column(length=50)
+    private String title;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String priority;
@@ -31,5 +30,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-    
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("O campo title dever conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
 }
